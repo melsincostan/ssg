64f77efb5501bc65ae26a324748fc2885d5231a8
Mélusine Hermier <melusine.hermier@proton.me>
2024-02-24 19:03:36 +0100

    make clean better

--- src/clean/clean.rs
+++ src/clean/clean.rs
@@ -4,23 +4,7 @@ use std::fs;
 mod infos;
 
 pub fn run() {
-    infos::files()
-        .iter()
-        .for_each(|e| {
-            let err = fs::remove_file(e).err();
-            if err.is_some() {
-                println!("Couldn't delete file {e}: {}", err.unwrap().to_string());
-            }
-        });
-    infos::folders()
-        .iter()
-        .for_each(|e| {
-            let err = fs::remove_dir(e).err();
-            if err.is_some() {
-                println!("Couldn't delete directory {e}: {}", err.unwrap().to_string());
-            }
-        });
-    let err = fs::remove_dir(infos::BASE_DIRECTORY).err();
+    let err = fs::remove_dir_all(infos::BASE_DIRECTORY).err();
     if err.is_some() {
         println!("Couldn't delete main directory {}: {}", infos::BASE_DIRECTORY, err.unwrap().to_string());
     }
